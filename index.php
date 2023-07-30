58785768850cb33d378dc2fa628e484a5226afd9
Ezaz Ahamed <ezazahamed3@gmail.com>
2023-07-31 01:08:37 +0600

    FavIcon Added

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ include 'connect.php';
   <head>
     <meta charset="UTF-8">
     <title> Responsive Registration Form</title>
+    <link rel="shortcut icon" href="./favicon.png" type="image/x-icon">
     <link rel="stylesheet" href="style.css">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
    </head>
